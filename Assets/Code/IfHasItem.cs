73b3936ae0d25ff8a0919a2e3bb7846d1a30fec4
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-07-07 18:07:38 -0400

    Added the childKiller

--- Assets/Code/IfHasItem.cs
+++ Assets/Code/IfHasItem.cs
@@ -3,20 +3,34 @@ using UnityEngine;
 public class IfHasItem : MonoBehaviour
 {
     public string itemToCheck = ""; // The item name to check in the inventory
-
+    public GameObject objectToSpawn;
     private void OnMouseDown()
     {
+       
+
         InventoryManager inventoryManager = FindObjectOfType<InventoryManager>();
 
         // Check if the specified item is in the inventory
         if (inventoryManager.HasItem(itemToCheck))
         {
+            spawnOject();
+            ChildKiller();
             Debug.Log("Player has the " + itemToCheck + " in their inventory!");
             // Perform any actions or logic specific to having the item
         }
         else
         {
             Debug.Log("Player does not have the " + itemToCheck + " in their inventory.");
+        }
+    }
+    private void spawnOject()
+    {
+        GameObject newObject = Instantiate(objectToSpawn);
+    }
+    private void ChildKiller()
+    {
+        foreach (Transform child in transform) {
+            Destroy(child.gameObject);
         }
     }
 }
