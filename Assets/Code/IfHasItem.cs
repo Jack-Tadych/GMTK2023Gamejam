df29bf4f88bc82d1355cc0e0a07705acb845f14a
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-07-07 19:11:55 -0400

    Added distance as factor

--- Assets/Code/IfHasItem.cs
+++ Assets/Code/IfHasItem.cs
@@ -4,26 +4,38 @@ public class IfHasItem : MonoBehaviour
 {
     public string itemToCheck = ""; // The item name to check in the inventory
     public GameObject objectToSpawn;
+    public float maxDistance = 2f; // Maximum distance allowed for picking up the item
     private void OnMouseDown()
     {
        
 
         InventoryManager inventoryManager = FindObjectOfType<InventoryManager>();
-
+        
         // Check if the specified item is in the inventory
-        if (inventoryManager.HasItem(itemToCheck))
+         GameObject playerObject = GameObject.FindWithTag("Player");
+        if (playerObject != null)
         {
-            ChildKiller();
-            spawnOject();
-            
+            Vector3 playerPosition = playerObject.transform.position;
+            // Check if the player is within the maximum distance
+            float distance = Vector3.Distance(transform.position, playerPosition);
+            if (distance <= maxDistance)
+            {
+                if (inventoryManager.HasItem(itemToCheck))
+                {
+                    ChildKiller();
+                    spawnOject();
+                    
 
-            Debug.Log("Player has the " + itemToCheck + " in their inventory!");
-            // Perform any actions or logic specific to having the item
+                    Debug.Log("Player has the " + itemToCheck + " in their inventory!");
+                    // Perform any actions or logic specific to having the item
+                }
+                else
+                {
+                    Debug.Log("Player does not have the " + itemToCheck + " in their inventory.");
+                }
+            }
         }
-        else
-        {
-            Debug.Log("Player does not have the " + itemToCheck + " in their inventory.");
-        }
+        
     }
     private void spawnOject()
     {
