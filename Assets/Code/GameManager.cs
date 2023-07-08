a7ba1afc69889b125835404b834227c37091ce38
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-07-08 00:07:36 -0400

    Tried the inventory

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,34 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private bool isKnifeClicked = false;
+    private InventoryManager inventoryManager;
+    private string inventoryFilePath = "inventory.txt";
 
+    private void Start()
+    {
+        inventoryManager = FindObjectOfType<InventoryManager>();
+
+        // Add some sample items to the inventory
+        // Save the inventory at the start of the game
+       
+
+        // Populate the inventory from the saved file
+        
+    }
+    
     private void Update()
     {
-        // Check for mouse click
-        if (Input.GetMouseButtonDown(0))
+        // Get the inventory items and print their names
+        List<Item> inventoryItems = inventoryManager.GetInventoryItems();
+        foreach (Item item in inventoryItems)
         {
-            // Cast a ray from the mouse position
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            // Check if the ray hits the knife object
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.CompareTag("Knife"))
-                {
-                    // Knife object is clicked
-                    isKnifeClicked = true;
-                    Debug.Log("Knife has been clicked!");
-                }
-            }
+            Debug.Log("Item Name: " + item.GetItemName());
         }
-    }
-
-    // Example function to check if the knife has been clicked
-    public bool IsKnifeClicked()
-    {
-        return isKnifeClicked;
     }
 }
--- Assets/Code/InventoryManager.cs
+++ Assets/Code/InventoryManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
@@ -10,6 +11,7 @@ public class InventoryManager : MonoBehaviour
     {
         inventoryItems.Add(item);
         Debug.Log("Item added to inventory: " + item.name);
+        randomShit();
     }
 
     public void RemoveItem(Item item)
@@ -17,27 +19,61 @@ public class InventoryManager : MonoBehaviour
         inventoryItems.Remove(item);
         Debug.Log("Item removed from inventory: " + item.name);
     }
-    
-    public bool HasItem(string itemToCheck)
-    {
-        
+
+    public bool HasItem(string itemToCheck){
         foreach (Item item in inventoryItems)
         {
-            if(item != null){
+            if (item != null)
+            {
                 print(item.GetItemName());
                 if (itemToCheck == item.GetItemName())
                 {
                     RemoveItem(item);
                     return true;
                 }
-            
-                
             }
-        
-        
         }
         return false;
     }
 
-    
+    public List<Item> GetInventoryItems() {
+        return inventoryItems;
+    }
+
+    public void SaveInventory(string filePath)
+    {
+        string[] lines = new string[inventoryItems.Count];
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            lines[i] = inventoryItems[i].GetItemName();
+        }
+        File.WriteAllLines(filePath, lines);
+        Debug.Log("Inventory saved to file: " + filePath);
+    }
+
+    public void PopulateInventory(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            inventoryItems.Clear();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string itemName = lines[i];
+                // Create new item object based on the name and add it to the inventory
+                Item newItem = new Item(itemName);
+                inventoryItems.Add(newItem);
+            }
+            Debug.Log("Inventory populated from file: " + filePath);
+        }
+        else
+        {
+            Debug.Log("Inventory file not found: " + filePath);
+        }
+    }
+
+    private void randomShit()
+    {
+        // Placeholder method
+    }
 }
