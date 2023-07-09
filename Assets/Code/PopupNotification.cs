b3ab6da20da14df8e60752147ac96bf5d31df0ee
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-07-09 05:21:53 -0400

    h

--- Assets/Code/PopupNotification.cs
+++ Assets/Code/PopupNotification.cs
@@ -7,7 +7,14 @@ public class PopupNotification : MonoBehaviour
     private GameObject popupNotification; // Reference to the Popup Notification child object
     private Text popupText; // Reference to the Text component of the Popup Notification
     private Image popupImage; // Reference to the Image component of the Popup Notification
+    private Image  popupPannal; 
 
+
+    private GameObject popupQestion;
+    private Button popupQuestionYes;
+    private Button popupQuestionNO;
+
+    
     private void Start()
     {
         // Find the Canvas object in the scene
@@ -17,39 +24,35 @@ public class PopupNotification : MonoBehaviour
         {
             // Find the Popup Notification child object
             popupNotification = canvasObject.transform.Find("PopupNotification").gameObject;
+            popupQestion = canvasObject.transform.Find("Panel").gameObject;
+
+            
 
             if (popupNotification != null)
             {
+                      print("Help");
                 // Find the Text and Image components of the Popup Notification
                 popupText = popupNotification.GetComponentInChildren<Text>();
                 popupImage = popupNotification.GetComponentInChildren<Image>();
+                popupPannal = popupNotification.GetComponentInChildren<Image>();
+     
+            }
+            if (popupQestion != null){
+                popupQuestionYes = popupNotification.GetComponentInChildren<Button>();
+                popupQuestionNO = popupNotification.GetComponentInChildren<Button>();
+                 
+            }
 
-                if (popupText != null && popupImage != null)
-                {
-                    // Change the text of the Popup Notification
-                    // Debug.Log("popupText and popupImage found");
-                }
-                else
-                {
-                    Debug.LogError("Text or Image component not found in Popup Notification!");
-                }
-            }
-            else
-            {
-                Debug.LogError("Popup Notification object not found!");
-            }
         }
-        else
-        {
-            Debug.LogError("Canvas object not found!");
-        }
+
 
         HidePopupPanel(); // Hide the Popup Panel by default
     }
+
     private void Update(){
         if (Input.GetKeyDown(KeyCode.V)){
           HidePopupPanel();
-          print("LOng live palistaine");
+          HidequestionPannel();
         }
     }
     public void setPopupText(string text, Sprite sprite)
@@ -61,6 +64,23 @@ public class PopupNotification : MonoBehaviour
       
     }
 
+
+    public void HidequestionPannel(){
+        popupQestion.SetActive(false);
+    }
+    
+    
+    public void Yes()
+    {
+        Debug.Log("Yes");
+    }
+
+    public void No()
+    {
+        Debug.Log("No");
+    }
+
+    
     private void ChangePopupImage(Sprite sprite)
     {
         if (popupNotification != null)
